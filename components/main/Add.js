349fab1596fec6d42e399275718d09a600776ae7
limitedgit <jameswang0505@hotmail.com>
2021-12-13 04:10:53 -0800

    added image picker

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,16 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Button, Image } from 'react-native';
 import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
 export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
+  const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
   const [camera, setCamera] = useState(null);
   const [image, setImage] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
 
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      const cameraStatus = await Camera.requestPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+
+      const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      setHasGalleryPermission(galleryStatus.status === 'granted');
+  
+     
     })();
   }, []);
 
@@ -21,10 +28,25 @@ export default function App() {
     }
   }
 
-  if (hasPermission === null) {
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  if (hasCameraPermission === null || hasGalleryPermission === null) {
     return <View />;
   }
-  if (hasPermission === false) {
+  if (hasCameraPermission === false || hasGalleryPermission === false) {
     return <Text>No access to camera</Text>;
   }
   return (
@@ -51,6 +73,10 @@ export default function App() {
           <Button
           title = "Take Picture"
           onPress={()=> takePicture()}
+         />
+         <Button
+          title = "Choose Image from Photos"
+          onPress={()=> pickImage()}
          />
           {image && <Image source={{uri: image}}
           style = {{flex: 1}}/>}
